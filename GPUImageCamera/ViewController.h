0d8528948c501004b1dde96b8275251aa2d34741
黄炜 <hw1@meitu.com>
2019-02-22 16:40:02 +0800

    初始化single view app

---
+++ GPUImageCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUImageCamera
+//
+//  Created by HW on 2019/2/22.
+//  Copyright © 2019 meitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GPUImageCamera/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GPUImageCamera
+//
+//  Created by HW on 2019/2/22.
+//  Copyright © 2019 meitu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
