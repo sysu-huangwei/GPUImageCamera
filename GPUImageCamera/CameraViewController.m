9e48913d3b4e8bae497a1ad6225ffbbbb3c12c5e
黄炜 <hw1@meitu.com>
2019-02-26 15:53:27 +0800

    完善拍照页逻辑

--- GPUImageCamera/CameraViewController.m
+++ GPUImageCamera/CameraViewController.m
@@ -9,10 +9,16 @@
 #import "CameraViewController.h"
 #import "CameraController.h"
 
+typedef NS_ENUM (NSInteger, CurrentState) {
+    CurrentStateTakingPhoto,//预览
+    CurrentStateAfterTaking,//拍照
+};
+
 @interface CameraViewController ()
 @property (strong, nonatomic) IBOutlet UIView *showView; //用于展示的view，下面的imageView会在这个view里
 @property (strong, nonatomic) CameraController* cameraController;  //相机控制器
 @property (strong, nonatomic) GPUImageView* imageView;
+@property (assign) CurrentState currentState;
 @end
 
 @implementation CameraViewController
@@ -20,6 +26,8 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+    
+    _currentState = CurrentStateTakingPhoto;
     
     //初始化相机控制器
     _cameraController = [[CameraController alloc] init];
@@ -51,10 +59,18 @@
  拍照
  */
 - (IBAction)takePhoto:(id)sender {
-    [_cameraController takePhotoWithcompletion:^(UIImage *processedImage, NSError *error) {
-        [self->_cameraController stop];
-    }];
-    
+    if (_currentState == CurrentStateTakingPhoto) {
+        [_cameraController takePhotoWithcompletion:^(UIImage *processedImage, NSError *error) {
+            self->_currentState = CurrentStateAfterTaking;
+            [self->_cameraController stop];
+            GPUImagePicture* picture = [[GPUImagePicture alloc] initWithImage:processedImage];
+            [picture addTarget:self->_imageView];
+            [picture useNextFrameForImageCapture];
+            [picture processImage];
+            [picture removeAllTargets];
+            picture = nil;
+        }];
+    }
 }
 
 
@@ -63,7 +79,14 @@
  返回上级界面
  */
 - (IBAction)back:(id)sender {
-    [self dismissViewControllerAnimated:YES completion:nil];
+    if (_currentState == CurrentStateTakingPhoto) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+    else {
+        _currentState = CurrentStateTakingPhoto;
+        [_cameraController open];
+    }
+    
 }
 
 @end
