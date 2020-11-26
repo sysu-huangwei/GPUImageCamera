b10c023826ebfb1a736979097337dce543de822b
rayyyhuang <rayyyhuang@tencent.com>
2020-11-26 21:26:22 +0800

    提交c++版本的softLight滤镜

---
+++ GPUImageCamera/OpenGL/MTFilterSoftLight.hpp
@@ -0,0 +1,32 @@
+//
+//  MTFilterSoftLight.hpp
+//  GPUImageCamera
+//
+//  Created by rayyy on 2020/11/26.
+//  Copyright © 2020 meitu. All rights reserved.
+//
+
+#ifndef MTFilterSoftLight_hpp
+#define MTFilterSoftLight_hpp
+
+#include "MTFilterBase.hpp"
+
+class MTFilterSoftLight : public MTFilterBase {
+public:
+    MTFilterSoftLight();
+    ~MTFilterSoftLight();
+    virtual void init();
+    virtual unsigned render();
+    
+    void setSrcTextureID(unsigned srcTextureID);
+    void setOverlayTextureID(unsigned overlayTextureID);
+    
+private:
+    int inputImageTextureUniform, overlayTextureUniform;
+    unsigned srcTextureID, overlayTextureID;
+    float softLightAlpha;
+    int softLightAlphaUniform;
+};
+
+
+#endif /* MTFilterSoftLight_hpp */
