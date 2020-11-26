32754da5e9c799ae24529779adefae86b0b590b9
rayyyhuang <rayyyhuang@tencent.com>
2020-11-26 14:48:23 +0800

    提交OpenGL基础方法

---
+++ GPUImageCamera/OpenGL/BaseDefine.h
@@ -0,0 +1,32 @@
+//
+// Created by HW on 2020/4/20.
+//
+
+#ifndef SMARTKIT_BASEDEFINE_H
+#define SMARTKIT_BASEDEFINE_H
+
+
+#if defined(WIN32) || defined(_WIN32) || defined(_WIN32_) || defined(WIN64) || defined(_WIN64) || defined(_WIN64_)
+#define PLATFORM_WINDOWS 1 //Windows平台
+#elif defined(ANDROID) || defined(_ANDROID_)
+#define PLATFORM_ANDROID 1 //Android平台
+#elif defined(__linux__)
+#define PLATFORM_LINUX	 1 //Linux平台
+#elif defined(__APPLE__) || defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR) || defined(TARGET_OS_MAC)
+#define PLATFORM_IOS	 1 //iOS、Mac平台
+#else
+#define PLATFORM_UNKNOWN 1
+#endif
+
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(x) { if (x) delete (x); (x) = NULL; }
+#endif
+#ifndef SAFE_DELETE_ARRAY
+#define SAFE_DELETE_ARRAY(x) { if (x) delete [] (x); (x) = NULL; }
+#endif
+#ifndef SAFE_FREE
+#define SAFE_FREE(p) if(p != NULL) {free(p); p = NULL;}
+#endif
+
+
+#endif //SMARTKIT_BASEDEFINE_H
