3dca97e9a55455a0e61304b7bd0ddd0b803512d7
rayyyhuang <rayyyhuang@tencent.com>
2020-11-26 20:10:30 +0800

    提交c++版本的Gradient滤镜

---
+++ GPUImageCamera/OpenGL/MTFilterGradient.hpp
@@ -0,0 +1,31 @@
+//
+//  MTFilterGradient.hpp
+//  GPUImageCamera
+//
+//  Created by rayyy on 2020/11/26.
+//  Copyright © 2020 meitu. All rights reserved.
+//
+
+#ifndef MTFilterGradient_hpp
+#define MTFilterGradient_hpp
+
+#include "MTFilterBase.hpp"
+
+class MTFilterGradient : public MTFilterBase {
+public:
+    MTFilterGradient();
+    ~MTFilterGradient();
+    virtual void init();
+    virtual void resize(int width, int height);
+    virtual unsigned render();
+    
+    void setSrcTextureID(unsigned srcTextureID);
+    
+private:
+    int inputImageTextureUniform;
+    unsigned srcTextureID;
+    float textureWidthOffset, textureHeightOffset;
+    int textureWidthOffsetUniform, textureHeightOffsetUniform;
+};
+
+#endif /* MTFilterGradient_hpp */
